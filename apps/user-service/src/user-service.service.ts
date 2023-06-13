7f65cddbf111580908b63140782b8ca652f59db4
lijianru <2219269374@qq.com>
2023-06-13 18:07:05 +0800

    translate to monorepo

--- apps/user-service/src/user-service.service.ts
+++ apps/user-service/src/user-service.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AppService {
+export class UserServiceService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World! User-service!';
   }
 }
